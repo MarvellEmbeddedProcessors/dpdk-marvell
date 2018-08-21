eef7f3734c3f31cd8ba8e6f6f3560aedd0d10374
Natalie Samsonov <nsamsono@marvell.com>
2018-08-21 18:33:52 +0300

    devtools: add lport doc to filter-branch.sh script
    Change-Id: I2b9a4ff079eca0c4066d69f0012d318c707152bb
    Signed-off-by: Natalie Samsonov <nsamsono@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/59335
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- devtools/filter-branch.sh
+++ devtools/filter-branch.sh
@@ -4,5 +4,6 @@
 git filter-branch --force --index-filter '\
 git rm -rf --cached --ignore-unmatch drivers/net/lport \
 git rm -rf --cached --ignore-unmatch drivers/net/vport \
+git rm -rf --cached --ignore-unmatch doc/guides/nics/lport.rst \
 ' --prune-empty @ b885c7a..HEAD
 
